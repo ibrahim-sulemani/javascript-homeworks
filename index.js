7d7d4decb3a3b283cad7989765123809f148041a
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-23 00:42:32 +0200

    Kreirajte variabli od site tipovi i napravete soodvetna proverka od koj tip e istata.

---
+++ index.js
@@ -0,0 +1,45 @@
+
+// Kreirajte variabli od site tipovi i napravete soodvetna proverka od koj tip e istata.
+
+//numbers
+let n = 10;
+console.log(typeof n);
+
+//strings
+let x = "hello";
+console.log(typeof x);
+
+//booleans
+let y = true;
+console.log(typeof y);
+
+//undefined
+let z;
+console.log(typeof z);
+
+//null
+let a = null;
+console.log(typeof a);
+
+//bigint
+let b = 123n;
+console.log(typeof b);
+
+//symbol
+let c = Symbol("hello");
+console.log(typeof c);
+
+//object
+let d = {
+  name: "Bill",
+  age: 30,
+};
+console.log(typeof d);
+
+//array
+let e = [1, 2, 3];
+console.log(typeof e);
+
+//regexp
+let f = /hello/;
+console.log(typeof f);

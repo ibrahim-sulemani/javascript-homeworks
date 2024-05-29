2940c1e87d2ce6b36062b7410106ab490d3685fe
Ibrahim Sulemani <sulemani.ibrahim@gmail.com>
2024-05-29 21:52:03 +0200

    solution of homework-4/exercise-2

---
+++ index.js
@@ -0,0 +1,10 @@
+// 2. Da se kreira promenliva so random broj,
+// i istata da se zaokruzi so funkciite od   Math (ceil, floor, round, trunc).
+
+let x = Math.random();
+console.log("Random broj: " + x);
+
+console.log("So funkcijata math.ceil dobivame brojot: " + Math.ceil(x));
+console.log("So funkcijata math.floor dobivame brojot: " + Math.floor(x));
+console.log("So funkcijata math.round dobivame brojot: " + Math.round(x));
+console.log("So funkcijata math.trunc dobivame brojot: " + Math.trunc(x));
